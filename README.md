985a032a69526a2496c5e4e86d179d09499c4bcd
henrique_home <henrique@poli.ufrj.br>
2020-03-20 18:09:41 -0300

    Add a versao julia. Algumas pequena modificacao na versao python.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Cálculo dos paramentros b e c da curva Weibull de uma distruibuição 
 com a media e desvio padrão conhecidos.
+
+Versão do código em:
+
+Python
+Fortran
+Julia
--- python/Weibull.py
+++ python/Weibull.py
@@ -7,7 +7,7 @@ def media(c,b):
     '''
     Media apartir dos paramentros c e b
     '''
-    return c*mt.gamma(1+1.0/b)
+    return c*mt.gamma(1.e0+1.e0/b)
 # **********************************************************
 
 # **********************************************************
@@ -17,7 +17,7 @@ def desv(c,b):
     '''
     mu = media(c,b)
 
-    dev = mt.sqrt((c**2)*mt.gamma(1+2.0/b) - mu**2)  
+    dev = mt.sqrt((c**2)*mt.gamma(1.e0+2.e0/b) - mu**2)  
 
     return dev
 # **********************************************************
@@ -67,8 +67,8 @@ def get_b_c(mu,sig,tol=1.e-11,maxIt=10000):
 # linha 2
         A[1] = c*c*mt.gamma(y) # A22
 # linha 3  
-        A[2] = -2.0            # A33
-        A[3] =  1.0            # A23
+        A[2] = -2.e0            # A33
+        A[3] =  1.e0            # A23
 
     def VetorF(F,sig,mu):
         '''
@@ -95,11 +95,11 @@ def get_b_c(mu,sig,tol=1.e-11,maxIt=10000):
     x = np.zeros(3,dtype=np.float64)
     F = np.zeros(3,dtype=np.float64)
 
-    alf   = 0.5
+    alf   = 0.5e0
 # ... chute inicial
     x[0] = mu     # c
     x[1] = sig/mu # y
-    x[2] = 2*x[1] # x
+    x[2] = 2.e0*x[1] # x
 
     VetorF(F,sig,mu)
 
@@ -110,7 +110,7 @@ def get_b_c(mu,sig,tol=1.e-11,maxIt=10000):
 # ... residuo
         R = F - matvec(A,x)
 # ... ||R||    
-        Res = mt.sqrt(np.dot(R,R))
+        Res = np.linalg.norm(R)
         if  Res < tol:       
             break
 # ...
@@ -120,11 +120,12 @@ def get_b_c(mu,sig,tol=1.e-11,maxIt=10000):
 # ... update x
         x = alf*x + (1.e0 - alf)*x0 
 
+    print('\n***************************************')
     print('Res final {0} para it {1}'.format(Res,i))
-
+    print('***************************************')
 
     c = x[0]
-    b = 2/x[1]
+    b = 2.e0/x[1]
     
     return b,c
 # **********************************************************
@@ -139,21 +140,25 @@ def main():
     mu  =99884.95e0
     sig =255.84e0
 
+#    mu  = 4.0
+#    sig = 2.0
+
     time  = tm.time()
-    b, c = get_b_c(mu,sig,tol=1e-11,maxIt=10000000)
+    b, c = get_b_c(mu,sig,tol=1e-10,maxIt=1500000)
     time = tm.time() - time
 
-    print('***************************************')
-    print('\nTime (s) =',time)
+    print('\n***************************************')
+    print('Time (s) =',time)
     print('***************************************')
 
 
-    print('***************************************')
+    print('\n***************************************')
     print('c = {0}\nb = {1}'.format(c,b))
     print('***************************************\n')
 
     mui  = media(c,b)
     sig1 = desv(c,b)
+    
     print('***************************************')
     print('mu    = {0}\nmui   = {1}\n|res| = {2}\n'.format(mu ,mui,abs(mu-mui)))
     print('sig   = {0}\nsigi  = {1}\n|res| = {2}\n'.format(sig,sig1,abs(sig-sig1)))
